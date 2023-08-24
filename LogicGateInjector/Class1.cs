9fde7ad649859d1554c8a6932b197825036c7398
N2O4 <97482716+DinitrogenTetroxide@users.noreply.github.com>
2023-08-24 12:49:40 +0200

    Add C# part

---
+++ LogicGateInjector/Class1.cs
@@ -0,0 +1,158 @@
+﻿using UnityEngine;
+using System;
+using SFS.Parts.Modules;
+
+namespace LogicGateInjector
+{
+    public class LogicGateModule : MonoBehaviour
+    {
+        public InputTrigger[] inputs;
+
+        public GameObject[] outputs;
+
+        public GateType gate;
+
+        public void SetAllOutputs(bool value, float mult = 1f)
+        {
+            foreach (GameObject go in outputs)
+            {
+                if (go)
+                    go.transform.localPosition = new Vector3(go.transform.localPosition.x, value ? 0f : (0.1f * mult), go.transform.localPosition.z); ;
+            }
+        }
+
+        public void Update()
+        {
+            switch (gate)
+            {
+                case GateType.Wire:
+                    bool found = false;
+                    foreach (var input in inputs) 
+                    {
+                        if (input)
+                        {
+                            if (input.enab)
+                            {
+                                found = true;
+                            }
+                        }
+                    }
+                    SetAllOutputs(found);
+                    break;
+
+                case GateType.AND:
+                    SetAllOutputs(inputs[0].enab && inputs[1].enab);
+                    break;
+
+                case GateType.OR:
+                    SetAllOutputs(inputs[0].enab || inputs[1].enab);
+                    break;
+                
+                case GateType.NOT:
+                    SetAllOutputs(!inputs[0].enab);
+                    break;
+
+                case GateType.XOR:
+                    SetAllOutputs(inputs[0].enab ^ inputs[1].enab);
+                    break;
+
+                case GateType.MagnetOutput:
+                    bool foundInp = false;
+                    foreach (var input in inputs)
+                    {
+                        if (input)
+                        {
+                            if (input.enab)
+                            {
+                                foundInp = true;
+                            }
+                        }
+                    }
+                    SetAllOutputs(foundInp, 20f);
+                    break;
+            }
+        }
+    }
+
+    public enum GateType
+    {
+        AND,
+        NOT,
+        OR,
+        XOR,
+        Wire,
+        Output,
+        MagnetOutput
+    }
+
+    public class OutputModule : MonoBehaviour
+    {
+        public GameObject modelEnabled;
+        public GameObject modelDisabled;
+
+        public InputTrigger input;
+
+        public void Update()
+        {
+            if (input.enab)
+            {
+                modelEnabled.SetActive(true);
+                modelDisabled.SetActive(false);
+            } else
+            {
+                modelEnabled.SetActive(false);
+                modelDisabled.SetActive(true);
+            }
+        }
+    }
+
+
+
+    [RequireComponent(typeof(Rigidbody2D), typeof(CircleCollider2D))]
+    public class InputTrigger : MonoBehaviour 
+    {
+        private Rigidbody2D rb2d;
+        private int layer;
+        private CircleCollider2D trigger;
+
+        public bool enab;
+
+        public int inputID;
+
+        public LogicGateModule lgm;
+        
+
+        private void Start()
+        {
+            rb2d = GetComponent<Rigidbody2D>();
+            rb2d.isKinematic = true;
+            trigger = GetComponent<CircleCollider2D>();
+            layer = LayerMask.NameToLayer("Docking Trigger");
+
+            if (lgm)
+                lgm.inputs[inputID] = this;
+        }
+
+        public void FixedUpdate()
+        {
+            
+        }
+
+        public void OnTriggerEnter2D(Collider2D other)
+        {
+            if (lgm.gate == GateType.MagnetOutput && other.GetComponentInChildren<DockingPortTrigger>() != null) 
+            {
+                return;
+            } // Stop the magnets from interfering with the DP converter input 
+            enab = true;
+        }
+        public void OnTriggerExit2D(Collider2D other)
+        {
+            if (lgm.gate == GateType.MagnetOutput && other.GetComponentInChildren<DockingPortTrigger>() != null)
+            {
+                return;
+            } // Stop the magnets from interfering with the DP converter input 
+            enab = false;
+        }
+    }
+}
